2b7faf12ba45ce2131e813588796614cc2dbda5a
david@idgames.net <david@idgames.net@d82cb8c8-a02b-11de-9858-1998a7f0ce10>
2009-09-13 12:15:18 +0000

    adding version number

--- trunk/hai/imaple/Core.java
+++ trunk/hai/imaple/Core.java
@@ -1,21 +1,29 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Haiiiiiiii faggot
  */
 
 package imaple;
+
+//~--- non-JDK imports --------------------------------------------------------
 
 import imaple.ex.CoreyIsAFaggotException;
 import imaple.ex.ICantCompileException;
 import imaple.ex.RageZoneException;
 import imaple.ex.YouAreTooFuckedException;
+
 import imaple.network.NIOHandler;
 import imaple.network.PacketCreator;
+
 import imaple.util.TimerManager;
+
+//~--- JDK imports ------------------------------------------------------------
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 import java.net.InetSocketAddress;
+
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.logging.Level;
@@ -27,110 +35,107 @@ import java.util.zip.ZipFile;
  * @author David
  */
 public class Core {
-    public static MainWindow window;
-    public static final HashMap<DataFileType, ZipFile> data =
-        new LinkedHashMap<DataFileType, ZipFile>();
+    public static String IP = "127.0.0.1";
+    public static int PORT = 8484;
+    public static final HashMap<DataFileType, ZipFile> data = new LinkedHashMap<DataFileType, ZipFile>();
     public static final boolean dbgMode = true;
     public static final NIOHandler networkHandler = new NIOHandler();
-    public static int PORT = 8484;
-    public static long lastPing;
-    public static String IP = "127.0.0.1";
-    public static BufferedReader jin;
     public static boolean fullscreen = false;
+    public static final int VERSION;
+    public static BufferedReader jin;
+    public static long lastPing;
+    public static MainWindow window;
 
     public static enum DataFileType {
-	CHARACTER,
-	EFFECT,
-	ETC,
-	ITEM,
-	LIST,
-	MAP,
-	STRING,
-	UI,
-	MOB,
-	TAMINGMOB,
-	SKILL,
-	QUEST,
-	REACTOR,
-	NPC,
-	MORPH,
-	SOUND
+        CHARACTER, EFFECT, ETC, ITEM, LIST, MAP, STRING, UI, MOB, TAMINGMOB, SKILL, QUEST, REACTOR, NPC, MORPH, SOUND
     }
 
     public static final void printDbg(String line) {
-	if (dbgMode) {
+        if (dbgMode) {
             System.err.println(line);
-	}
+        }
     }
 
     private static void initiallizeData() throws YouAreTooFuckedException {
-	DataFileType[] types = DataFileType.values();
-	for (int x = 0; x < types.length; x++) {
+        DataFileType[] types = DataFileType.values();
+
+        for (int x = 0; x < types.length; x++) {
             StringBuilder fName = new StringBuilder(types[x].name().toLowerCase());
+
             fName.replace(0, 1, String.valueOf(Character.toUpperCase(fName.charAt(0))));
+
             try {
-		String fNameAsString = fName.toString() + ".IDA";
-		printDbg("[IDALoader] Loading: " + fNameAsString);
-		data.put(types[x], new ZipFile(fNameAsString));
+                String fNameAsString = fName.toString() + ".IDA";
+
+                printDbg("[IDALoader] Loading: " + fNameAsString);
+                data.put(types[x], new ZipFile(fNameAsString));
             } catch (IOException zafuckz) {
-		throw new YouAreTooFuckedException(zafuckz);
+                throw new YouAreTooFuckedException(zafuckz);
             }
         }
     }
 
     public static final int itoa(String in) {
-	return Integer.parseInt(in);
+        return Integer.parseInt(in);
     }
 
     public static void main(String[] main) throws YouAreTooFuckedException, RageZoneException {
-	for (int i = 0; i < main.length; i++) {
-            if (main[i].equalsIgnoreCase("-ip") && i < main.length - 1) {
-		IP = main[i + 1];
+        for (int i = 0; i < main.length; i++) {
+            if (main[i].equalsIgnoreCase("-ip") && (i < main.length - 1)) {
+                IP = main[i + 1];
             }
-            if (main[i].equalsIgnoreCase("-port") && i < main.length - 1) {
-		try {
+
+            if (main[i].equalsIgnoreCase("-port") && (i < main.length - 1)) {
+                try {
                     PORT = itoa(main[i + 1]);
-		} catch (NumberFormatException ragezone) {
+                } catch (NumberFormatException ragezone) {
                     throw new RageZoneException(ragezone);
-		}
+                }
             }
+
             if (main[i].equalsIgnoreCase("-fs")) {
-		fullscreen = FullScreenDevice.isDisplayChangeAvailable() && FullScreenDevice.isExclusiveModeAvailable();
+                fullscreen = FullScreenDevice.isDisplayChangeAvailable() && FullScreenDevice.isExclusiveModeAvailable();
             }
-	}
-	try {
+        }
+
+        try {
             networkHandler.connectTo(new InetSocketAddress(IP, PORT));
-	} catch (YouAreTooFuckedException yatfe) {
+        } catch (YouAreTooFuckedException yatfe) {
             System.err.println("Cannot connect to the server.");
             yatfe.printStackTrace();
             System.exit(1);
-	}
-	printDbg("Connected to Login Server.");
-	try {
+        }
+
+        printDbg("Connected to Login Server.");
+
+        try {
             initiallizeData();
         } catch (YouAreTooFuckedException omgwtfbbq) {
             omgwtfbbq.printStackTrace();
-	}
-	window = new MainWindow();
-	window.initiallize(fullscreen);
-	window.setVisible(true);
-	jin = new BufferedReader(new InputStreamReader(System.in));
-	if (fullscreen) {
+        }
+
+        window = new MainWindow();
+        window.initiallize(fullscreen);
+        window.setVisible(true);
+        jin = new BufferedReader(new InputStreamReader(System.in));
+
+        if (fullscreen) {
             FullScreenDevice.setFullScreen(window);
             FullScreenDevice.changeDisplayMode(FullScreenDevice.msDisplayMode);
-	}
+        }
     }
-    
+
     public NIOHandler getSession() {
         return networkHandler;
     }
 
     public void pingReceived() {
-	lastPing = System.currentTimeMillis();
+        lastPing = System.currentTimeMillis();
     }
 
     public void sendPong() {
-	final long then = System.currentTimeMillis();
+        final long then = System.currentTimeMillis();
+
         try {
             getSession().sendPacket(PacketCreator.getPong());
         } catch (CoreyIsAFaggotException ex) {
@@ -138,20 +143,72 @@ public class Core {
         } catch (ICantCompileException ex) {
             Logger.getLogger(Core.class.getName()).log(Level.SEVERE, null, ex);
         }
-	TimerManager.getInstance().schedule(new Runnable() {
+
+        TimerManager.getInstance().schedule(new Runnable() {
             @Override
             public void run() {
-		try {
+                try {
                     if (lastPing - then < 0) {
-			if (getSession().isConnected()) {
+                        if (getSession().isConnected()) {
                             System.out.print("The pong packet has not succesfully been sent to the server.");
                             System.exit(1);
-			}
+                        }
                     }
-		} catch (NullPointerException e) {
+                } catch (NullPointerException e) {
+
                     // Client has already been closed.
-		}
+                }
             }
-	}, 15000);
+        }, 15000);
     }
+
+	static {
+		VERSION = Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt(
+				String.valueOf(
+				Integer.parseInt("1")))))))))))))))))))))))))))))))))))))))))))))));
+	}
 }
