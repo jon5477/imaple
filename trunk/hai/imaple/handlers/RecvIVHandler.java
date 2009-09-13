bd39032e2f46fb2cf5706acad80d0bcf1abae08c
david@idgames.net <david@idgames.net@d82cb8c8-a02b-11de-9858-1998a7f0ce10>
2009-09-13 12:53:42 +0000

    revivhandler wtf??

---
+++ trunk/hai/imaple/handlers/RecvIVHandler.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package imaple.handlers;
+
+import imaple.network.JPacketHandler;
+import imaple.network.input.SeekableLittleEndianAccessor;
+
+/**
+ *
+ * @author David
+ */
+public class RecvIVHandler implements JPacketHandler {
+	public void handle(SeekableLittleEndianAccessor slea) {
+		//Wtf goes here
+	}
+}
