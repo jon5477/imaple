f5bf1aebd48cd89b458787ee11eafadc71593f34
kenta_iwasaki1@yahoo.com <kenta_iwasaki1@yahoo.com@d82cb8c8-a02b-11de-9858-1998a7f0ce10>
2009-09-17 10:10:36 +0000

    huh

--- trunk/hai/imaple/canvas/AbstractCanvas.java
+++ trunk/hai/imaple/canvas/AbstractCanvas.java
@@ -8,45 +8,54 @@ package imaple.canvas;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import javax.swing.JButton;
 
 /**
  *
  * @author David
  */
 public abstract class AbstractCanvas {
-	public int getRenderDelay() {
-		return 10;
-	}
+    public int getRenderDelay() {
+	return 10;
+    }
 
-	public int getLogicDelay() {
-		return 30;
-	}
+    public int getLogicDelay() {
+	return 30;
+    }
 
-	public void onMousePressed(MouseEvent evt) {
+    public void onMousePressed(MouseEvent evt) {
+        String button = ((JButton)evt.getSource()).getText();
+        if (button.equals("Login")) {
+            String userName = LoginScreen.userField.getText();
+            String passWord = LoginScreen.passField.getText();
+        } else if (button.equals("Register")) {
 
-	}
+        } else if (button.equals("Website")) {
 
-	public void onMouseReleased(MouseEvent evt) {
+        }
+    }
 
-	}
+    public void onMouseReleased(MouseEvent evt) {
 
-	public void onMouseMoved(MouseEvent evt) {
+    }
 
-	}
+    public void onMouseMoved(MouseEvent evt) {
 
-	public void onMouseDragged(MouseEvent evt) {
+    }
 
-	}
+    public void onMouseDragged(MouseEvent evt) {
 
-	public void onKeyPressed(KeyEvent evt) {
+    }
 
-	}
+    public void onKeyPressed(KeyEvent evt) {
 
-	public void onKeyReleased(KeyEvent evt) {
+    }
+
+    public void onKeyReleased(KeyEvent evt) {
 		
-	}
+    }
 
-	public abstract void doLogics();
+    public abstract void doLogics();
 
-	public abstract void doRender(Graphics2D holyFuck);
+    public abstract void doRender(Graphics2D g);
 }
